6e1d30cfbdba4a6b03b48c8c6a6442759adeac0c
Vipin <vipin@cbio.mskcc.org>
2013-11-12 10:05:26 -0800

    checked the compactibility of GFFParser module

--- gfftools/gff_to_bed_conv.py
+++ gfftools/gff_to_bed_conv.py
@@ -6,60 +6,58 @@ BED format typically represents the transcript models.
 Usage: python gff_to_bed_conv.py in.gff > out.bed  
 """
 
-import sys
 import re
+import sys
+import GFFParser
 
-def stop_err(fmsg):
+def WriteBED(tinfo):
+    """
+    writing result files in bed format 
+    """
+    for ent1 in tinfo:
+        for idx, tid in enumerate(ent1['transcripts']):
+            #print tid[0]
+            #print ent1['exons'][idx]
+            
+        break 
     
-    sys.stderr.write('%s\n' % fmsg)
-    sys.exit(-1)
-
-def WriteBED(tinfo, einfo, bed_fh):
-    """BED writter"""
-    
-    for contig_id, feature in tinfo.items():
-        for tid, tloc in feature.items():
-            if tid in einfo: # get corresponding exon info 
-                exon_cnt, exon_len, exon_cod, fex, rstart = 0, '', '', 0, None
-                if tloc[-1] == '-':
-                    if einfo[tid][0][1] > einfo[tid][-1][1]:einfo[tid].sort()
-                for ex_ele in einfo[tid]:
-                    if ex_ele[0] != contig_id:continue
-                    exon_cnt += 1
-                    exon_len += str(int(ex_ele[2])-int(ex_ele[1])+1) + ','
-                    if fex == 0: # calculate the relative exon start 
-                        exon_cod += '0,'
-                        fex = 1
-                        rstart = int(ex_ele[1])
-                    else:
-                        exon_cod += str(int(ex_ele[1])-rstart) + ','
-                if exon_len:
-                    pline = [str(contig_id),
-                            tloc[0],
-                            tloc[1],
-                            tid,
-                            tloc[2],
-                            tloc[-1],
-                            tloc[0],
-                            tloc[1],
-                            '0',
-                            str(exon_cnt),
-                            exon_len,
-                            exon_cod]
-                    bed_fh.write('\t'.join(pline) + '\n')
-    bed_fh.close()
+#    for contig_id, feature in tinfo.items():
+#        for tid, tloc in feature.items():
+#            if tid in einfo: # get corresponding exon info 
+#                exon_cnt, exon_len, exon_cod, fex, rstart = 0, '', '', 0, None
+#                if tloc[-1] == '-':
+#                    if einfo[tid][0][1] > einfo[tid][-1][1]:einfo[tid].sort()
+#                for ex_ele in einfo[tid]:
+#                    if ex_ele[0] != contig_id:continue
+#                    exon_cnt += 1
+#                    exon_len += str(int(ex_ele[2])-int(ex_ele[1])+1) + ','
+#                    if fex == 0: # calculate the relative exon start 
+#                        exon_cod += '0,'
+#                        fex = 1
+#                        rstart = int(ex_ele[1])
+#                    else:
+#                        exon_cod += str(int(ex_ele[1])-rstart) + ','
+#                if exon_len:
+#                    pline = [str(contig_id),
+#                            tloc[0],
+#                            tloc[1],
+#                            tid,
+#                            tloc[2],
+#                            tloc[-1],
+#                            tloc[0],
+#                            tloc[1],
+#                            '0',
+#                            str(exon_cnt),
+#                            exon_len,
+#                            exon_cod]
+#                    bed_fh.write('\t'.join(pline) + '\n')
 
 def ParseAnno(gff_fh):
     """Reading GFF3 file to get feature annotation"""
 
     tinfo, einfo = dict(), dict()
-    features = ['transcript', 'scRNA', 'mRNA', 'ncRNA', 'miRNA', 'rRNA', 'snoRNA', 'snRNA', 'tRNA', 'pseudogenic_transcript'] ## TODO: Growing list of RNA species. 
 
     for gff_line in gff_fh:
-        gff_line = gff_line.strip('\n\r').split('\t')
-        if re.match(r'#', gff_line[0]) or re.match(r'>', gff_line[0]) or len(gff_line) == 1:continue ## not considering commented and FASTA  lines from GFF
-        if '' in gff_line:continue ## empty fields in any line ? 
-        assert len(gff_line) == 9, '\t'.join(gff_line) ## a valid GFF line contains only 9 tab-delimited fields
 
         if gff_line[2] in features:
             tid = None
@@ -86,24 +84,27 @@ def ParseAnno(gff_fh):
 
 def __main__():
 
-    cmd_arg = OptionParser()
-    cmd_arg.add_option('', '-q', dest='query_file', help='Data in GFF3 file')
-    cmd_arg.add_option('', '-o', dest='result_file', help='BED file name')
+    try:
+        query_file = sys.argv[1]
+    except:
+        print __doc__
+        sys.exit(-1)
 
-    options, args = cmd_arg.parse_args()
-    if len(sys.argv) < 2:cmd_arg.print_help();sys.exit(-1)
+    Transcriptdb = GFFParser.Parse(query_file)  
 
-    if options.query_file != None and options.result_file != None: 
-        try:
-            gff_fh = open(options.query_file, 'rU')
-        except Exception, erm:
-            stop_err('Error reading query file ' + str(erm))
-        
-        tinfo, einfo = ParseAnno(gff_fh) ## get transcript annotation from GFF3 file 
-        try:
-            bed_fh = open(options.result_file, 'w')
-        except Exception, erm:
-            stop_err('Error writing result file ' + str(erm))
-        WriteBED(tinfo, einfo, bed_fh) ## BED writter 
+    WriteBED(Transcriptdb)
+
+#    if options.query_file != None and options.result_file != None: 
+#        try:
+#            gff_fh = open(options.query_file, 'rU')
+#        except Exception, erm:
+#            stop_err('Error reading query file ' + str(erm))
+#        
+#        tinfo, einfo = ParseAnno(gff_fh) ## get transcript annotation from GFF3 file 
+#        try:
+#            bed_fh = open(options.result_file, 'w')
+#        except Exception, erm:
+#            stop_err('Error writing result file ' + str(erm))
+#        WriteBED(tinfo, einfo, bed_fh) ## BED writter 
 
 if __name__ == "__main__": __main__() 
