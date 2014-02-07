ca8720e04538688f759aec2d2cbf8ee9123491dc
vipints <vipin@cbio.mskcc.org>
2014-02-07 12:15:37 -0800

    updated- fastq-dump run on the downloaded .sra file and split it

--- fetch_remote_data/ncbi_sra_run_download.py
+++ fetch_remote_data/ncbi_sra_run_download.py
@@ -9,12 +9,14 @@ import sys
 import re, os
 import urllib2
 import shutil
+import subprocess 
 
 base_url = "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/"
 
 try:
     RUNID = sys.argv[1]
     download_path = sys.argv[2]
+    lib_type = sys.argv[3]
 except:
     print __doc__
     sys.exit(-1)
@@ -61,18 +63,45 @@ base_url = base_url + RUNID[0:9] + '.sra'
 if not os.path.exists(download_path):
     os.makedirs(download_path)
 
+## getting the absolute path 
+download_path = os.path.realpath(download_path)
+
+## out file name 
+out_file_name = download_path + '/' + RUNID[0:9] + '.sra' 
+
 ## creating temporory file 
-tempfile=open(download_path + "/" + RUNID[0:9] + '.sra', "wb")
+tempfile=open(out_file_name, "wb")
 
 ## start fetching the remote file 
-sra_file = urllib2.urlopen(base_url)
+try:
+    sra_file = urllib2.urlopen(base_url, timeout=1 )
+except urllib2.URLError, err:
+    raise MyException("There was an error: %r" % e)
 
 ## download job starts 
 sys.stdout.write('\tdownloading ' + base_url + ' ... \n')
 shutil.copyfileobj(sra_file, tempfile)
-sys.stdout.write('\tsaved at ' + download_path + '/' + RUNID[0:9] + '.sra done! \n')
+sys.stdout.write('\tsaved at ' + out_file_name + ' done! \n')
 
 tempfile.close()
 sra_file.close()
 
+## split the file based on SE/PE
+# TODO fix the path of SRA toolkit 
+os.environ['PATH'] += os.pathsep + '/share/software/sratoolkit/sratoolkit.2.3.1-centos_linux64/bin/'
 
+## depends on the compress type and library protocol type
+if lib_type in ['pe', 'PE', 'paired-end']:
+    cli = 'fastq-dump --gzip --split-3 --outdir %s %s' % (download_path, out_file_name) 
+elif lib_type in ['se', 'SE', 'single-end']:
+    cli = 'fastq-dump --gzip --outdir %s %s' % (download_path, out_file_name)
+else:
+    print 'Error! Library layout [PE/pe/paired-end|SE/se/single-end]'
+    print '\tYour Library layout is ', lib_type
+    print '\tProgram cannot continue, Exiting...'
+    sys.exit(-1)
+
+## split the .SRA format file based on the library layout
+sys.stdout.write('\trun ' + cli + '\n')
+process = subprocess.Popen(cli, shell=True) 
+process.wait()
