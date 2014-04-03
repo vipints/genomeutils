ba2790422b39b700cb6aacf1cdfd2c99d2638837
vipints <vipin@cbio.mskcc.org>
2014-04-03 11:54:06 -0700

    -small util for accessing and saving compressed pickled files

---
+++ utils/compressed_pickle.py
@@ -0,0 +1,50 @@
+"""
+save and load compressed pickled objects in python 
+"""
+
+import bz2 
+import sys 
+import cPickle
+
+def save(filename, myObj):
+    """
+    save the object to a file using pickle
+
+    @args filename: result file name 
+    @type filename: str 
+    @args myObj: object to save 
+    @type myObj: python pickleable object
+    """
+
+    try:
+        fh = bz2.BZ2File(filename, "wb")
+    except IOError, details:
+        sys.stderr.write('File %s cannot be written\n' % filename)
+        sys.stderr.write(details) 
+        return 
+
+    cPickle.dump(myObj, fh, protocol=2) 
+    fh.close()
+
+
+def load(filename):
+    """
+    load a pickled file
+
+    @args filename: name of the file to load 
+    @type filename: str
+    """
+    
+    try:
+        fh = bz2.BZ2File(filename, 'rb') 
+    except, IOError, details:
+        sys.stderr.write('File %s cannot be read\n' % filename)
+        sys.stderr.write(details) 
+        return 
+
+    myObj = cPickle.load(fh) 
+    fh.close() 
+
+    return myObj
+        
+
