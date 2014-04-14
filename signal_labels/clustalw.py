1cc058bd33e68e7b5e842d379fbf3c2476c3e490
vipints <vipin@cbio.mskcc.org>
2014-04-14 09:10:04 -0700

    allows you to run a multiple sequence alignment with ClustalW2 using the default options

--- signal_labels/clustalw.py
+++ signal_labels/clustalw.py
@@ -1,33 +1,51 @@
+#!/usr/bin/env python 
 """
+Program to run clustalw2 
+
+Usage: python clustalw.py infile outfile data_type out_format 
+
+INFILE: FASTA
+DATATYPE: DNA, PROTEIN 
+OUTFORM: CLUSTAL, PHYLIP, FASTA
+
+Requirement: 
+    install clustalw2 and set the path correctly 
+
+TODO: include more commandline features to clustalw2
+check the inputs from user 
 """
 
 import os
 import sys
-import shutil
 import tempfile
 import subprocess
 
-class ClustalWrun:
+def ClustalWrun(infile, outfile, data_type, outform):
     """
+    clustalw2 run 
     """
-    def __init__(self,opts=None):
-        self.opts = opts
-        self.iname = 'infile_copy'
-        shutil.copy(self.opts.input,self.iname) 
-
-    def run(self):
-        tlf = open(self.opts.outlog,'w')
-        cl = ['clustalw2 -INFILE=%s -OUTFILE=%s -OUTORDER=%s -TYPE=%s -OUTPUT=%s' % (self.iname,self.opts.output,self.opts.out_order,self.opts.dnarna,self.opts.outform)]
-        if self.opts.seq_range_end <> None and self.opts.seq_range_start <> None:
-            cl.append('-RANGE=%s,%s' % (self.opts.seq_range_start,self.opts.seq_range_end))
-        if self.opts.outform=='CLUSTAL' and self.opts.outseqnos <> None:
-            cl.append('-SEQNOS=ON')
-        process = subprocess.Popen(' '.join(cl), shell=True, stderr=tlf, stdout=tlf)
-        rval = process.wait()
     
+    outlog = "clustalw2_run.log"
+    tlf = open(outlog,'w')
 
-proc = ClustalWrun(opts)
-proc.run()
-    
-            
+    out_order = "INPUT" # ALIGNED
+
+    cl = ['clustalw2 -INFILE=%s -OUTFILE=%s -OUTORDER=%s -TYPE=%s -OUTPUT=%s' % (infile, outfile, out_order, data_type, outform)]
+    process = subprocess.Popen(' '.join(cl), shell=True, stderr=tlf, stdout=tlf)
+    rval = process.wait()
+    tlf.close() 
+
+
+try:
+    INFILE = sys.argv[1]
+    OUTFILE = sys.argv[2]
+    DATATYPE = sys.argv[3]
+    OUTFORM = sys.argv[4]
+except:
+    print __doc__ 
+    sys.exit(-1) 
+
+
+
+ClustalWrun(INFILE, OUTFILE, DATATYPE, OUTFORM) 
 
