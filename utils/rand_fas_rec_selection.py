f4a9acaa6dbe9d28e6fd657b3754a02caf7fe1dd
vipin <vipin@cbio.mskcc.org>
2014-08-14 15:55:27 -0400

    subsample the fasta record labels for different experiment setup

---
+++ utils/rand_fas_rec_selection.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""
+select random fasta records from a fasta file. 
+
+"""
+
+import sys
+import random
+from Bio import SeqIO
+
+
+def __main__():
+
+    try:
+        inh = open(sys.argv[1], "rU")
+        fasta_out = open(sys.argv[2], 'w')
+    except:
+        print __doc__
+        sys.exit(-1)
+
+    total_record_count = 4100 # 40000
+    sub_sample_records = 4000 # 12000
+
+    label_type = "+1" # -1 
+
+    try:
+        accept_prob = (1.0*sub_sample_records)/total_record_count
+    except:
+        accept_prob = 1
+
+    cnt = 1
+    fas_rec = 0 
+
+    for rec in SeqIO.parse(inh, 'fasta'):
+        desc = rec.description.split(" ")
+
+        if desc[1]==label_type:
+            rnb = random.random()
+
+            if rnb <= accept_prob:
+                fasta_out.write(rec.format("fasta"))
+
+                if cnt == sub_sample_records:
+                    break 
+
+                cnt += 1 
+        fas_rec += 1 
+
+    inh.close()
+    fasta_out.close()
+
+    print
+    print '%d number of records scanned' % fas_rec 
+    print '%d number of labels dumped' % cnt 
+    print 
+
+
+if __name__=="__main__": __main__()
