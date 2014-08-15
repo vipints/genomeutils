35c28c0e581c9f18ed8671a835793e77ddfae1f1
vipin <vipin@cbio.mskcc.org>
2014-08-15 12:16:40 -0400

    genomic position nucleotide frequenccy vizualization

---
+++ signal_labels/nt_freq_viz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+"""
+
+"""
+
+""
+take individual position on the label sequence and count the occurence of each nucleotide 
+
+plot the high frequncy of the nucleotide to that position 
+
+
+""
