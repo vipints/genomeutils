b1993e6528a4850e8e0426a81d6729607d538a98
vipin <vipin@cbio.mskcc.org>
2014-08-19 16:43:20 -0400

    organized the print statement

--- utils/tskm_pred_filter.py
+++ utils/tskm_pred_filter.py
@@ -2,10 +2,11 @@
 """
 Filter genes predicted by TranscriptSkimmer program  
 
-Usage: python tskm_pred_filter.py
+Usage: python tskm_pred_filter.py in.gff > filter.gff 
 """
 
 import sys 
+import random 
 import collections
 from gfftools import GFFParser
 from bx.intervals.cluster import ClusterTree
@@ -37,6 +38,7 @@ def __main__():
     clean_entries = dict() 
     for chrom, sub_trees in cluster_trees.items():
         for start, stop, id in sub_trees.getregions():
+            
             for xq in range(1):
 
                 try:
@@ -46,23 +48,57 @@ def __main__():
  
     for rec in gff_content:
         if rec['name'] in clean_entries:
-            print '%s\t%s\tgene\t%d\t%d\t.\t%s\t.\tID=%s;Name=%s' % (rec['chr'], rec['source'], rec['start'], rec['stop'], rec['strand'], rec['name'], rec['name'])
+            print '%s\t%s\tgene\t%d\t%d\t.\t%s\t.\tID=%s;Name=%s' % (rec['chr'], 
+                    rec['source'], 
+                    rec['start'], 
+                    rec['stop'], 
+                    rec['strand'], 
+                    rec['name'], 
+                    rec['name'])
+
             for idx, tid in enumerate(rec['transcripts']):
                 t_start = rec['exons'][idx][0][0]
                 t_stop = rec['exons'][idx][-1][-1]
-                print '%s\t%s\t%s\t%d\t%d\t.\t%s\t.\tID=%s;Parent=%s' % (rec['chr'], rec['source'], rec['transcript_type'][idx], t_start, t_stop, rec['strand'], tid[0], rec['name'])
+                print '%s\t%s\t%s\t%d\t%d\t.\t%s\t.\tID=%s;Parent=%s' % (rec['chr'], 
+                        rec['source'], 
+                        rec['transcript_type'][idx], 
+                        t_start, t_stop, 
+                        rec['strand'], 
+                        tid[0], 
+                        rec['name'])
                 
                 for ex_cod in rec['utr5_exons'][idx]:
-                    print '%s\t%s\tfive_prime_UTR\t%d\t%d\t.\t%s\t.\tParent=%s' % (rec['chr'], rec['source'], ex_cod[0], ex_cod[1], rec['strand'], tid[0])
+                    print '%s\t%s\tfive_prime_UTR\t%d\t%d\t.\t%s\t.\tParent=%s' % (rec['chr'], 
+                        rec['source'], 
+                        ex_cod[0], 
+                        ex_cod[1], 
+                        rec['strand'], 
+                        tid[0])
 
                 for ex_cod in rec['cds_exons'][idx]:
-                    print '%s\t%s\tCDS\t%d\t%d\t.\t%s\t%d\tParent=%s' % (rec['chr'], rec['source'], ex_cod[0], ex_cod[1], rec['strand'], ex_cod[2], tid[0])
+                    print '%s\t%s\tCDS\t%d\t%d\t.\t%s\t%d\tParent=%s' % (rec['chr'], 
+                        rec['source'], 
+                        ex_cod[0], 
+                        ex_cod[1], 
+                        rec['strand'], 
+                        ex_cod[2], 
+                        tid[0])
 
                 for ex_cod in rec['utr3_exons'][idx]:
-                    print '%s\t%s\tthree_prime_UTR\t%d\t%d\t.\t%s\t.\tParent=%s' % (rec['chr'], rec['source'], ex_cod[0], ex_cod[1], rec['strand'], tid[0])
+                    print '%s\t%s\tthree_prime_UTR\t%d\t%d\t.\t%s\t.\tParent=%s' % (rec['chr'], 
+                        rec['source'], 
+                        ex_cod[0], 
+                        ex_cod[1], 
+                        rec['strand'], 
+                        tid[0])
 
                 for ex_cod in rec['exons'][idx]:
-                    print '%s\t%s\texon\t%d\t%d\t.\t%s\t.\tParent=%s' % (rec['chr'], rec['source'], ex_cod[0], ex_cod[1], rec['strand'], tid[0])
+                    print '%s\t%s\texon\t%d\t%d\t.\t%s\t.\tParent=%s' % (rec['chr'], 
+                        rec['source'], 
+                        ex_cod[0], 
+                        ex_cod[1], 
+                        rec['strand'], 
+                        tid[0])
 
 
 if __name__ == "__main__":
