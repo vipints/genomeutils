81beb138deca2508a539263e043ff5282b433ee8
vipin <vipin@cbio.mskcc.org>
2014-08-20 16:35:30 -0400

    data parsing part is over

--- signal_labels/nt_freq_viz.py
+++ signal_labels/nt_freq_viz.py
@@ -1,12 +1,45 @@
 #!/usr/bin/env python 
 """
+Map the nucleotide frequency at individual position of a label sequence.
 
+Usage: python nt_freq_viz.py label.fa 
 """
 
-""
-take individual position on the label sequence and count the occurence of each nucleotide 
+import sys 
+from Bio import SeqIO 
+from collections import defaultdict
 
-plot the high frequncy of the nucleotide to that position 
+
+def __main__():
+    """
+    """
+
+    try:
+        fas_file = sys.argv[1]
+    except:
+        print __doc__
+        sys.exit(-1) 
+
+    label_seq_length = 2400  # please change according to the label length 
+    nuc_freq_table = defaultdict() 
+
+    for xq in range(label_seq_length):
+        nuc_freq_table[xq] = dict(A=0, T=0, C=0, G=0) 
+       
+    for rec in SeqIO.parse(fas_file, "fasta"):
+        for xp, ntd in enumerate(rec.seq):
+            nuc_freq_table[xp][ntd] += 1
+        
+    pos_pref = defaultdict() # max frequency nucleotide in the label sequence positions 
+    for xp, freq in nuc_freq_table.items():
+        freq_nt = max(freq, key=freq.get) 
+        
+        pos_pref[xp] = {freq_nt : freq[freq_nt]}
+        break  
+
+    # TODO plot function 
 
 
-""
+
+if __name__ == "__main__":
+    __main__() 
