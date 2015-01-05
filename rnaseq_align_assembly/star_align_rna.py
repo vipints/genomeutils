e95b5449ef0633470f59a6c54339281ebccc6009
Vipin <vipin@cbio.mskcc.org>
2015-01-05 18:09:53 -0500

    wrapper script for STAR program

---
+++ rnaseq_align_assembly/star_align_rna.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+"""
+Program to align rnaseq transcriptome reads to the genome using STAR aligner 
+
+Requirement: 
+    STAR - https://github.com/alexdobin/STAR/releases
+
+"""
+
+def run_star_alignment():
+    """
+    """
