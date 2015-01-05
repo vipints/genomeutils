be251303859a2a03f512c24caa05bd18e5b10bed
Vipin <vipin@cbio.mskcc.org>
2015-01-05 18:17:10 -0500

    wrapper for Transcriptome assembly program TSKM

---
+++ rnaseq_align_assembly/trsk_assembly.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+"""
+Program to run transcriptome assembly using TransriptSkimmer on read alignment data 
+
+Requirement: 
+    TransriptSkimmer - 
+
+"""
+
+def run_trsk():
+    """
+    """
