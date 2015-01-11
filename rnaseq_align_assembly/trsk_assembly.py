f0ebf98c109740b78b77fc666948cd8070a8b9b0
Vipin <vipin@cbio.mskcc.org>
2015-01-11 18:28:36 -0500

    skeleton work done

--- rnaseq_align_assembly/trsk_assembly.py
+++ rnaseq_align_assembly/trsk_assembly.py
@@ -7,6 +7,33 @@ Requirement:
 
 """
 
-def run_trsk():
+import os 
+import subprocess
+
+def run_trsk(org_name, gio_file, bam_file, res_path, gff_file="pred_genes.gff", reg_file="pred_region.bed"):
     """
+    run TransriptSkimmer for provided genome
+
+
     """
+
+    res_path = "%s/%s/trans_pred/" % (res_path, org_name)
+
+    max_exon_length = org_db
+    max_intron_length = org_db
+    max_intergenic_region = org_db
+
+    options="-maxel %d -ss -reglen 0.66 -maxic %d -minic 20 -maxin %d -mm 2 -exm 3 -indt 150 -exd 20 -tf 0.5 -inscf 3 -excut 3 -toff 100 -el 15" % (max_exon_length, max_intergenic_region, max_intron_length)
+
+
+    os.chdir(res_path) 
+
+    gio_file = org_db
+    trsk_path = org_db
+
+    cli = "%s./infer_genes -gio %s -bam %s -gff %s -reg %s %s" % (trsk_path, gio_file, bam_file, gff_file, reg_file, options)  
+    sys.stdout.write('\trun %s \n' % cli)
+
+    process = subprocess.Popen(cli, shell=True) 
+    process.wait()
+
