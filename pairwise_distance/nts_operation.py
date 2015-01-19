eb44359309980632c87412a2f1b3092a7d9c4009
Vipin <vipin@cbio.mskcc.org>
2015-01-19 15:33:41 -0500

    nucleotide operations

---
+++ pairwise_distance/nts_operation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python 
+
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+
+def U_to_T_nts(fasta_in, fasta_out):
+    """
+    change nucleotide U to T 
+
+    @args fasta_in: fasta file with "U" as a nucleotide 
+    @type fasta_in: str
+    @args fasta_out: result fasta file
+    @type fasta_out: str 
+    """
+
+    fout = open(fasta_out, "w")
+
+    for rec in SeqIO.parse(fasta_in, "fasta"):
+        mut_seq = rec.seq.tomutable()
+
+        for idx, nt in enumerate(mut_seq):
+            if nt.upper() == 'U':
+                mut_seq[idx]="T"
+            else:
+                mut_seq[idx] = nt.upper()
+
+        mut_seq = SeqRecord(mut_seq, id=rec.id, description=rec.description)
+        fasta_out.write(mut_seq.format("fasta"))
+
+    fasta_out.close()
