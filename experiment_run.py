694f9720f2b9cfa78206723997ab0044b7355251
Vipin <vipin@cbio.mskcc.org>
2015-01-30 15:42:20 -0500

    gtf download module read y

--- experiment_run.py
+++ experiment_run.py
@@ -23,7 +23,24 @@ def download_fasta(org_details):
         elif det['release_db'] == 'ensembl_metazoa':
             dld.fetch_ensembl_metazoa_fasta(det['release_num'], det['name'], det['fasta'])
         else:
-            print "download plugin for %s not available, module works with ensembl, ensembl_metazoa and phytozome." % det['release_db']
+            print "download fasta plugin for %s not available, module works with ensembl, ensembl_metazoa and phytozome." % det['release_db']
+
+
+def download_gtf(org_details):
+    """
+    download gtf/gff file from remote data publishing services
+    """
+
+    for org_name, det in org_details.items():
+        if det['release_db'] == 'ensembl':
+            dld.fetch_ensembl_gtf(det['release_num'], det['name'], det['gtf'])
+        elif det['release_db'] == 'phytozome':
+            dld.fetch_phytozome_gff(det['release_num'], det['name'], det['gtf']) 
+        elif det['release_db'] == 'ensembl_metazoa':
+            dld.fetch_ensembl_metazoa_gtf(det['release_num'], det['name'], det['gtf'])
+        else:
+            print "download gtf plugin for %s not available, module works with ensembl, ensembl_metazoa and phytozome." % det['release_db']
+
 
 
 if __name__=="__main":
@@ -36,7 +53,6 @@ if __name__=="__main":
 
     download_fasta(org_details) 
 
-    
+    download_gtf(org_details)     
    
-
 
