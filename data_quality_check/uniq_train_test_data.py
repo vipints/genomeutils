6afa139bc4ef71873d6ee6c57cf910316b328c4a
Vipin <vipin@cbio.mskcc.org>
2015-04-28 14:53:51 -0400

    modified the requirement tag

--- data_quality_check/uniq_train_test_data.py
+++ data_quality_check/uniq_train_test_data.py
@@ -5,7 +5,7 @@ From two different data set generate a set of training and testing labels withou
 Usage: python uniq_train_test_data.py 
 
 Requirement:
-
+    biopython:- http://biopython.org
 """
 
 import sys
@@ -14,7 +14,6 @@ from Bio import SeqIO
 def __main__():
     """
     """
-
     try:
         label_type_1 = sys.argv[1] # train data 
         label_type_2 = sys.argv[2] # test data 
@@ -22,10 +21,8 @@ def __main__():
         print __doc__
         sys.exit(-1) 
 
-
     train_recs = fasta_reader(label_type_1)
     test_recs = fasta_reader(label_type_2)
-
 
     shared_keys = set(train_recs.keys()) & set(test_recs.keys())
 
@@ -36,7 +33,6 @@ def fasta_reader(fasname):
     """
     read fasta file and return the records locations
     """
-    
     location_marks = dict() 
     for rec in SeqIO.parse(fasname, "fasta"):
 
