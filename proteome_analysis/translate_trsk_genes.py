750cf880b122caaa35d55744c81d1125aa7f6092
Vipin <vipin@cbio.mskcc.org>
2015-05-21 15:13:35 -0400

    fixed plotting of genes

--- proteome_analysis/translate_trsk_genes.py
+++ proteome_analysis/translate_trsk_genes.py
@@ -9,13 +9,13 @@ import numpy as np
 import pandas as pd 
 from Bio import SeqIO 
 from gfftools import GFFParser
+import matplotlib.pyplot as plt 
 
 
 def trsk_gene_len_dist(gtf_file):
     """
 
     """
-    
     anno_db = GFFParser.Parse(gtf_file) 
 
     cds_idx = [] # deleting the empty cds lines  
@@ -37,17 +37,22 @@ def trsk_gene_len_dist(gtf_file):
         trans_len[idx, 1] = cds_len
         genes.append(feat['name'])
     
+    ## gene, cds length information 
     df_len_dis_genes = pd.DataFrame(trans_len, columns=['gene_len', 'cds_len'], index=genes)
 
-    ## 500 1000 2000 5000 10000 20000 500000 for gene length  
-    for 
+    ## plotting the gene length based on the bins of gene length  
+    gene_length = trans_len[:,0] ## gene length from the matrix 
 
-   
-    
+    freq, bins = np.histogram(gene_length, bins=10, range=None, normed=False, weights=None)
+    bins = np.delete(bins, 10) 
 
-    #TODO making bins of gene length and cds length 
+    df_gene_len_bin = pd.DataFrame(freq, columns=['frequency'], index=bins) 
+    plt.figure() 
+
+    df_gene_len_bin.plot(kind="bar")
+    #plt.savefig()
 
 
 if __name__=="__main__":
     fname = "../../SRA-rnaseq/H_sapiens/trans_pred/H_sapiens_trsk_genes.gff"
-    get_trsk_genes(fname)
+    trsk_gene_len_dist(fname)
