66bbfc2787ec693e169f173fb0f4b2251a913243
Vipin <vipin@cbio.mskcc.org>
2015-05-21 11:09:58 -0400

    fixed a syntax typo

--- proteome_analysis/translate_trsk_genes.py
+++ proteome_analysis/translate_trsk_genes.py
@@ -11,11 +11,9 @@ from Bio import SeqIO
 from gfftools import GFFParser
 
 
-def get_trsk_genes(gtf_file):
+def trsk_gene_len_dist(gtf_file):
     """
-    parse the trsk predictions 
-    create the df with length 
-    only select the genes with CDS region 
+
     """
     
     anno_db = GFFParser.Parse(gtf_file) 
@@ -27,7 +25,7 @@ def get_trsk_genes(gtf_file):
 
     anno_db = np.delete(anno_db, cds_idx) 
     
-    trans_len = np.zeros(len(anno_db, 2))
+    trans_len = np.zeros((len(anno_db), 2))
     genes = [] 
 
     for idx, feat in enumerate(anno_db):
@@ -38,8 +36,16 @@ def get_trsk_genes(gtf_file):
         trans_len[idx, 0] = feat['stop']-feat['start']
         trans_len[idx, 1] = cds_len
         genes.append(feat['name'])
-        
+    
     df_len_dis_genes = pd.DataFrame(trans_len, columns=['gene_len', 'cds_len'], index=genes)
+
+    ## 500 1000 2000 5000 10000 20000 500000 for gene length  
+    for 
+
+   
+    
+
+    #TODO making bins of gene length and cds length 
 
 
 if __name__=="__main__":
