6ca518906a963653e885055968a6ed621b60031f
Vipin <vipin@cbio.mskcc.org>
2015-07-15 22:17:39 -0400

    fixed few more items for unit tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -21,11 +21,19 @@ class TestDataProcs(unittest.TestCase):
 
         self.data = {}
 
+    def testFetchPhytozomeGtf(self):
+
+        self.data = {}
+
+    def testFetchPhytozomeFasta(self):
+
+        self.data = {}
 
 def getTestSuite():
     """
     set up composite test suite
     """
+
     suite1 = unittest.TestLoader().loadTestsFromTestCase(TestDataProcs)
     return unittest.TestSuite([suite1,suite2])
 
