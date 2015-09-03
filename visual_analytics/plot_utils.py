9ad9838067dec0951c5e323914e57ce502c20f0e
Vipin <vipin@cbio.mskcc.org>
2015-09-03 17:04:29 -0400

    single method test performance plotting

--- visual_analytics/plot_utils.py
+++ visual_analytics/plot_utils.py
@@ -9,6 +9,84 @@ import cPickle
 from scipy.signal import argrelextrema
 
 
+def bar_chart_auroc(file)
+    """
+    plot a bar chart based on the test performance from diff CV
+    """
+
+    file = "hs_s32_trsk_tss.pickle"
+
+    ## loading the data 
+    data = data_process(file)
+
+    method = 'individual'
+    org_code = 'H_sapiens'
+    numb_cvs =  len(data[method][0][org_code])
+
+    ## fetching the best performance based on the validation and test 
+    best_eval = numpy.argmax(data[method][0][org_code], axis=1)
+    test_perf = numpy.zeros(numb_cvs) 
+    for idx, v_idx in enumerate(best_eval):
+        test_perf[idx] = data[method][1][org_code][idx][v_idx]
+
+    ## plot settings 
+    import pylab
+    pylab.figure(figsize=(3,4))
+    pylab.rcParams.update({'figure.autolayout':True})
+
+    offset = 0 
+    width = 0.10
+    seperator = 0.10 
+
+    xlocations = [] 
+    min_max = [] 
+    rects = [] 
+
+    x_axis_marks = []
+    for idk, ele in enumerate(test_perf):
+        xlocations.append(offset + 0.05)
+        min_max.append(ele) 
+        rects.append(pylab.bar(offset, ele, width, color="#88aa33", edgecolor="white"))
+        offset += width
+
+        idk += 1 
+        cvname = "fold_%d" % idk
+        x_axis_marks.append(cvname) 
+        
+    ## mean bar 
+    rects.append(pylab.bar(offset, test_perf.mean(), width, color="#ff9999", edgecolor="white")) 
+    xlocations.append(offset + 0.05)
+    offset += width
+
+    x_axis_marks.append('mean')
+
+    ## adjusting the axis range 
+    min_max.sort()
+    ymax = min_max[-1]*1.05 
+    ymin = min_max[0]*0.98
+
+    ## set the ticks
+    tick_step = 0.02 
+    ticks = [tick_step*i for i in xrange(int(round(ymax/tick_step)+1))]
+
+    pylab.yticks(ticks)
+    pylab.xticks(xlocations, x_axis_marks, rotation="vertical")
+
+    pylab.xlim(0, offset) 
+    pylab.ylim(ymin, ymax) 
+
+    plot_title = "title of the figure" 
+    pylab.title(plot_title)
+
+    pylab.gca().get_yaxis().tick_left()
+    pylab.gca().get_xaxis().tick_bottom()
+
+    pylab.gca().get_yaxis().grid(True)
+    pylab.gca().get_xaxis().grid(False)
+
+    fout_name ="test_performance_with_CV.pdf"
+    pylab.savefig(fout_name)
+
 
 def data_process(fname):
     """
