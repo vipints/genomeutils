d9acca77f1643e974f6098dc7a5267db60ab1aa0
Vipin <vipin@cbio.mskcc.org>
2016-01-14 17:36:34 -0500

    wilcoxon test function added

--- utils/stat_tests.py
+++ utils/stat_tests.py
@@ -50,6 +50,33 @@ def best_test_perf_with_eval(xv_result_file, methods=None, org_names=None):
     return best_test_perf
 
 
+def compute_wilcoxon_test(best_test_score):
+    """
+    """
+    
+    pairs_test = [('individual', 'union'), ('individual', 'mtl'), \
+        ('individual', 'mtmkl'), ('union', 'mtl'), ('union', 'mtmkl'), \
+        ('mtl', 'mtmkl')]
+    org_names = best_test_score.keys()
+
+    ##ttest_p_val = numpy.zeros((len(org_names), len(pairs_test)))
+    ttest_p_val = numpy.zeros((len(pairs_test), len(org_names)))
+
+    for org_idx, org_code in enumerate(org_names):
+        meth_perf = best_test_score[org_code]
+
+        for pair_idx, rel_pair in enumerate(pairs_test):
+            t_stats, p_val = stats.wilcoxon(meth_perf[rel_pair[0]], meth_perf[rel_pair[1]])
+            
+            ##ttest_p_val[org_idx, pair_idx] = p_val
+            ttest_p_val[pair_idx, org_idx] = p_val
+        
+    
+    ##df_pval = pandas.DataFrame(ttest_p_val, columns=pairs_test, index=org_names)
+    df_pval = pandas.DataFrame(ttest_p_val, columns=org_names, index=pairs_test)
+    
+    return df_pval 
+
 
 def compute_paired_ttest(best_test_score):
     """
