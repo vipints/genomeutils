343aad250de4605eb63b5faa82a3ec40fb690cda
Vipin <vipin@cbio.mskcc.org>
2016-07-08 09:49:36 -0400

    function to fetch protists data fetch added

--- fetch_remote_data/download_data.py
+++ fetch_remote_data/download_data.py
@@ -183,6 +183,69 @@ def fetch_phytozome_fasta(release_version, species_name, download_path):
     if not org_name_valid:
         exit("error: fasta file for %s is not present in phytozome release version %s URL %s/%s" % (species_name, release_version, base_url_fasta, species_name))
 
+def fetch_ensembl_protists_gtf(release_version, species_name, download_path):
+    ## check the url for getting the recent version of the repository 
+    base_url_gtf = "ftp://ftp.ensemblgenomes.org/pub/protists/release-%s/gtf/" % release_version 
+    
+    try:
+        org_file = urllib2.urlopen(base_url_gtf)
+    except urllib2.URLError, err_release:
+        exit("ensembl_release_version %s is NOT found for %s" % (release_version, species_name))
+
+    org_name_valid = False
+    for org_name in org_file:
+        org_name=org_name.strip("\n\r")
+
+        ## check the organism directory at ftp remote folder 
+        if org_name.split()[-1] != species_name: 
+            continue
+        
+        org_name_valid = True
+        ## updating the base url 
+        base_url_gtf = '%s%s/' % (base_url_gtf, org_name.split()[-1])
+
+        try:
+            gtf_files = urllib2.urlopen(base_url_gtf)
+        except urllib2.URLError, err_gtf:
+            exit("ensembl_release genome annotation missing" % base_url_gtf)
+
+        ## mapping to short names  Arabidopsis_thaliana --> A_thaliana
+        genus, species = species_name.strip().split("_")
+        org_short_name = "%s_%s" % (genus[0].upper(), species)
+
+        base_file_path = "%s/%s/ensembl_release_%s" % (download_path, org_short_name, release_version)
+        if not os.path.exists(base_file_path):
+            try:
+                os.makedirs(base_file_path)
+            except OSError:
+                exit("error: cannot create the directory %s." % base_file_path)
+            
+        for gtf_name in gtf_files:
+            gtf_name =gtf_name.strip('\n\r')
+
+            if re.search(r'.*.\d+.gtf.gz$', gtf_name.split()[-1]):
+                gtf_file = "%s/%s" % (base_file_path, gtf_name.split()[-1])
+                tempfile=open(gtf_file, "wb")
+
+                try:
+                    ftp_file=urllib2.urlopen(base_url_gtf+gtf_name.split()[-1])
+                except urllib2.URLError, err_file:
+                    exit(err_file)
+
+                sys.stdout.write('\tdownloading %s ...\n' % gtf_name.split()[-1])
+                shutil.copyfileobj(ftp_file, tempfile)
+                sys.stdout.write('\t... saved at %s \n' % gtf_file)
+
+                tempfile.close()
+                ftp_file.close()
+
+        gtf_files.close()
+    org_file.close()
+
+    if not org_name_valid:
+        print("error: fasta file for %s is not present in ensemblgenomes release version %s" % (species_name, release_version)) 
+        print("URL checked %s/%s" % (base_url_gtf, species_name))
+
 
 def fetch_ensembl_fungi_gtf(release_version, species_name, download_path):
     """
@@ -422,6 +485,73 @@ def fetch_ensembl_gtf(release_version, species_name, download_path):
         print 
         print "error: gtf file for %s is not present in ensembl release version %s" % (species_name, release_version)   
         print "URL checked %s/%s" % (base_url_gtf, species_name) 
+
+def fetch_ensembl_protists_fasta(release_version, species_name, download_path):
+    ## check the url for getting the recent version of the repository 
+    base_url_fasta = "ftp://ftp.ensemblgenomes.org/pub/protists/release-%s/fasta/" % release_version 
+
+    try:
+        org_file = urllib2.urlopen(base_url_fasta)
+    except urllib2.URLError, err_release:
+        exit("ensembl_fungi_release_version %s is NOT found for %s" % (release_version, species_name))
+
+    org_name_valid = False
+    for org_name in org_file:
+        org_name=org_name.strip("\n\r")
+        
+        ## check the organism directory at ftp remote folder 
+        if org_name.split()[-1] != species_name: 
+            continue
+
+        org_name_valid = True
+        ## updating the base url 
+        base_url_fasta = '%s%s/dna/' % (base_url_fasta, org_name.split()[-1])
+
+        try:
+            fa_files = urllib2.urlopen(base_url_fasta)
+        except urllib2.URLError, err_faseq:
+            exit("ensembl_protists_release genome sequence missing" % base_url_fasta)
+
+        ## mapping to short names  Arabidopsis_thaliana --> A_thaliana
+        genus, species = species_name.strip().split("_")
+        org_short_name = "%s_%s" % (genus[0].upper(), species)
+
+        base_file_path = "%s/%s/ensembl_release_%s" % (download_path, org_short_name, release_version)
+        if not os.path.exists(base_file_path):
+            try:
+                os.makedirs(base_file_path)
+            except OSError:
+                exit("error: cannot create the directory %s." % base_file_path)
+
+        for fa_name in fa_files:
+            fa_name =fa_name.strip('\n\r')
+
+            ## include repeatmasked genome 
+            if re.search(r'.*.dna.toplevel.fa.gz$', fa_name.split()[-1]) or \
+                re.search(r'.*.dna_rm.toplevel.fa.gz$', fa_name.split()[-1]) or \
+                re.search(r'.*.dna_sm.toplevel.fa.gz$', fa_name.split()[-1]):
+
+                fasta_file = "%s/%s" % (base_file_path, fa_name.split()[-1])
+                tempfile=open(fasta_file, "wb")
+
+                try:
+                    ftp_file=urllib2.urlopen(base_url_fasta+fa_name.split()[-1])
+                except urllib2.URLError, err_file:
+                    exit(err_file)
+
+                sys.stdout.write('\tdownloading %s ... \n' % fa_name.split()[-1])
+                shutil.copyfileobj(ftp_file, tempfile)
+                sys.stdout.write('\t... saved at %s \n' % fasta_file)
+                tempfile.close()
+                ftp_file.close()
+
+        fa_files.close()
+    org_file.close()
+
+    if not org_name_valid:
+        print("error: fasta file for %s is not present in ensembl release version %s" % (species_name, ensembl_release_version)) 
+        print("URL checked %s/%s" % (base_url_fasta, species_name))
+
 
 
 def fetch_ensembl_fungi_fasta(release_version, species_name, download_path):
